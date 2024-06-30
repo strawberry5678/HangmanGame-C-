f5e5416026e87a0bd130d3c88041dcfd3324860b
Lungile Shongwe <LungileShongwe@example.com>
2024-07-01 00:07:24 +0200

    first commit

---
+++ Hangman game/Hangman game/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace HangmanGame
+{
+    partial class HangmanForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.scoreboardListBox = new System.Windows.Forms.ListBox();
+            this.wordLabel = new System.Windows.Forms.Label();
+            this.incorrectLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.HangmanForm_KeyPress);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(59, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(532, 160);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "finish";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // scoreboardListBox
+            // 
+            this.scoreboardListBox.FormattingEnabled = true;
+            this.scoreboardListBox.ItemHeight = 16;
+            this.scoreboardListBox.Location = new System.Drawing.Point(255, 1);
+            this.scoreboardListBox.Name = "scoreboardListBox";
+            this.scoreboardListBox.Size = new System.Drawing.Size(120, 84);
+            this.scoreboardListBox.TabIndex = 3;
+            this.scoreboardListBox.SelectedIndexChanged += new System.EventHandler(this.scoreboardListBox_SelectedIndexChanged);
+            // 
+            // wordLabel
+            // 
+            this.wordLabel.AutoSize = true;
+            this.wordLabel.Location = new System.Drawing.Point(452, 89);
+            this.wordLabel.Name = "wordLabel";
+            this.wordLabel.Size = new System.Drawing.Size(70, 16);
+            this.wordLabel.TabIndex = 4;
+            this.wordLabel.Text = "wordLabel";
+            // 
+            // incorrectLabel
+            // 
+            this.incorrectLabel.AutoSize = true;
+            this.incorrectLabel.Location = new System.Drawing.Point(170, 98);
+            this.incorrectLabel.Name = "incorrectLabel";
+            this.incorrectLabel.Size = new System.Drawing.Size(44, 16);
+            this.incorrectLabel.TabIndex = 5;
+            this.incorrectLabel.Text = "label1";
+            // 
+            // HangmanForm
+            // 
+            this.ClientSize = new System.Drawing.Size(641, 381);
+            this.Controls.Add(this.incorrectLabel);
+            this.Controls.Add(this.wordLabel);
+            this.Controls.Add(this.scoreboardListBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "HangmanForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        
+        private System.Windows.Forms.Label wordlength;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtSubmitLetter;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox scoreboardListBox;
+        private System.Windows.Forms.Label wordLabel;
+        private System.Windows.Forms.Label incorrectLabel;
+    }
+}
+
---
+++ Hangman game/Hangman game/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Policy;
+using System.Windows.Forms;
+
+namespace HangmanGame
+{
+    /*Lungile Shongwe
+    1 Jnauary 2023
+    Purpose: The purpose for this project is to design a hangman game.
+    */
+    //The for this class is to combine data variables and functions into one unit.
+    public partial class HangmanForm : Form
+
+    {
+        //Creating an Array 
+     
+        private readonly string[] wordList = { "addition", "substraction", "multiplication", "division", "remainder" ,"equal", "mathematics","algorithm","formula","plus"}; //adding wors to the array .
+        private string currentWord;
+        private ArrayList correctGuesses; //Array list being created for the list of correct guesses being made .
+        private ArrayList incorrectGuesses;//array list being created for the list of incorrect guesses being made.
+        private SortedList<string, int> scoreboard; // Sorted list being created to store the scores .
+
+        private int maxWrongGuesses = 10; // Maximum number of guesses being made .
+
+        private int wrongGuessCount; // Variable used to count the number of wrong guesses .
+        public HangmanForm()
+        {
+            InitializeComponent();
+            InitializeGame();
+            incorrectLabel.Text = ""; // masking 
+        }
+
+        private void InitializeGame()
+        {
+            currentWord = SelectRandomWord();
+
+            correctGuesses = new ArrayList(); //Arraylist for the correctGuesses being created 
+
+            incorrectGuesses = new ArrayList(); //Arraylist for the incorrectGuessess being created
+
+              scoreboard = new SortedList<string, int>();  //sorted list being created for the scoreboard
+
+            wrongGuessCount = 0;
+
+            DisplayWord();
+            ScoreBoardDisplayed();
+        }
+        //Selecting random word 
+        private string SelectRandomWord() //function for the random word that will be  selected 
+        {
+            Random random = new Random();
+            return wordList[random.Next(wordList.Length)];
+        }
+
+        private void DisplayWord() //Input by user being compared.
+        {
+            wordLabel.Text = "";
+            foreach (char letter in currentWord)
+            {
+                if (correctGuesses.Contains(letter)) //If the current guessed letter is correct, letter is displayed
+                {
+                    wordLabel.Text += letter + " ";
+                }
+                else
+                {
+                    wordLabel.Text += "_ ";
+                }
+            }
+        }
+
+        private void ScoreBoardDisplayed() //scoreboard being displayed 
+        {
+            scoreboardListBox.Items.Clear();
+            foreach (var score in scoreboard)
+            {
+                   scoreboardListBox.Items.Add($"{score.Key}: {score.Value} guesses");
+            }
+        }
+
+        private void CheckGuess(char guess) //checking the guessed character  being inputed
+        {
+            if (currentWord.Contains(guess)) //checking to see if the correct letter has been guessed and it will be added to the word .
+            {
+                correctGuesses.Add(guess); //adding letter 
+            }
+            else //checking to see if the incorrect letter has been guessed 
+            {
+                incorrectGuesses.Add(guess); //adding letter
+                wrongGuessCount++;
+            }
+
+            DisplayWord();
+            DisplayIncorrectGuesses();
+
+            if (wrongGuessCount >= maxWrongGuesses) // Player makes more than 10 guesses game will end 
+            {
+                EndGame(false);
+            }
+            else if (!wordLabel.Text.Contains("_"))
+            {
+                EndGame(true);
+            }
+        }
+
+        private void DisplayIncorrectGuesses() // setting the incorrect letters aside.
+        {
+            incorrectLabel.Text = string.Join(", ", incorrectGuesses.Cast<char>()); //  a ',' being added to separate the letters  
+        }
+
+        private void EndGame(bool isWinner) //Determining current status for user
+        {
+            try
+            {
+
+                if (isWinner)
+                {
+                    MessageBox.Show("WOW,GREAT! You guessed the right word."); // Correct word is  guessed
+                    scoreboard.Add(currentWord, correctGuesses.Count);
+                    ScoreBoardDisplayed();
+                }
+                else
+                {
+                    MessageBox.Show($"OOPs, you ran out of guesses. The word was: {currentWord}"); // incorrect word being guessed
+                }
+               // ScoreBoardDisplayed();
+              //  InitializeGame();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Something went wrong: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        
+
+        }
+
+        private void HangmanForm_KeyPress(object sender, KeyPressEventArgs e) //keypress being used 
+        {
+            if (char.IsLetter(e.KeyChar)) 
+            {
+                char guess = char.ToLower(e.KeyChar); // letters are being displayed in lower case
+                if (!correctGuesses.Contains(guess) && !incorrectGuesses.Contains(guess))
+                {
+                    CheckGuess(guess);
+                }
+            }
+        }
+        private void RestartGame()
+        {
+            this.Hide(); //Hide the current HangmanForm form.
+            HangmanForm restart = new HangmanForm(); //Creates a new instance of the HangmanForm.
+            restart.ShowDialog(); //Displays the new instance of the HangmanForm.
+            this.Close(); //Closes the current HangmanForm form once the new one is displayed.
+        }
+       
+
+
+        private void button3_Click(object sender, EventArgs e) //Button for start
+        {
+            InitializeGame();
+            RestartGame();
+        }
+
+        private void button4_Click(object sender, EventArgs e) //Button for finish
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scoreboardListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ScoreBoardDisplayed();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        enum Bodyparts
+        {
+            Head,
+            Left_Eye,
+            Right_Eye,
+            Mouth,
+            Right_Arm,
+            Left_Arm,
+            Body,
+            Right_Leg,
+            Left_leg
+
+        }
+
+        void DrawHangPost()
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Brown, 10);
+            g.DrawLine(p, new Point(130, 218), new Point(130, 5));
+            g.DrawLine(p, new Point(135, 5), new Point(65, 5));
+            g.DrawLine(p, new Point(69, 0), new Point(60, 50));
+            DrawBodyPart(Bodyparts.Head);
+            DrawBodyPart(Bodyparts.Left_Eye);
+            DrawBodyPart(Bodyparts.Right_Eye);
+
+
+
+
+        }
+        void DrawBodyPart(Bodyparts bp)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Blue);
+            if (bp == Bodyparts.Head)
+                g.DrawEllipse(p, 40, 50, 40, 40);
+            else if(bp == Bodyparts.Left_Eye)
+            {
+                SolidBrush s = new SolidBrush(Color.Black);
+                g.FillEllipse(s, 50, 60, 5, 5);
+
+            }
+            else if (bp == Bodyparts.Right_Eye)
+            {
+                SolidBrush s = new SolidBrush(Color.Black);
+                g.FillEllipse(s, 50, 60, 5, 5);
+
+            }
+            else if (bp == Bodyparts.Mouth)
+            {
+                g.DrawArc(p, 50, 60, 20, 20, 45, 90);
+
+            }
+        }
+
+
+        private void Form1_shown(object sender, EventArgs e)
+        {
+            DrawHangPost();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string submitLwtter = txtSubmitLetter.Text;
+
+        }
+    }
+}*/
